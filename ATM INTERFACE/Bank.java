e773b3922eee8134c086ba0f9983dd905ad3d6c5
SANTHOSH KUMAR G <157136701+SANTHOSHKUMAR-G@users.noreply.github.com>
2024-03-20 22:52:59 +0530

    Bank

---
+++ ATM INTERFACE/Bank.java
@@ -0,0 +1,19 @@
+public class Bank {
+	  private int balance;
+	    public Bank(int balance) {
+	        this.balance = balance;
+	    }
+	    public void withdraw(int amt){
+	        if (balance < amt) {
+	           System.out.println("Insufficient fund");
+	        }
+	        balance -= amt;
+	    }
+	    public void deposit(int amt) {
+	        balance += amt;
+	    }
+	    public int getBalance() {
+	        return balance;
+	    }
+}
+
